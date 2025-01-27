654c7803b2df3d5ce362c0da5ecc7d2191ee2f7d
Kavindra2002 <108975209+Kavindra2002@users.noreply.github.com>
2025-01-27 12:20:23 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,37 @@
+package com.v2;
+
+public class Hello {
+
+// changes by kavindra
+
+     // Method for addition
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    // Method for subtraction
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+	//instance  variable  object creation is needed for 
+	
+	int i =10 ; // instance variabe  // instance  Heap memory   objects
+	static int  k =18; //static  //class area  method area   u need no to create any object 
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+   System.out.println(hello.i);
+      int j=15; //local variable  //stack
+      System.out.println(Hello.k);  // static  String organisation = "wipro"
+      
+      System.out.println(j);
+System.out.println("first changes in git ");
+System.out.println("Second  changes in git ");
+
+
+
+	}
+
+	
+
+}
